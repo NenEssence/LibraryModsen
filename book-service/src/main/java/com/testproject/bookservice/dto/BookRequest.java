cde5a3e20dfa3e482ee400e65ba824041d1b1357
Andrei Sheverinov <jobboxsh@mail.ru>
2024-11-01 10:53:08 +0300

    test(book-service): Добавлены unit тесты

--- book-service/src/main/java/com/testproject/bookservice/dto/BookRequest.java
+++ book-service/src/main/java/com/testproject/bookservice/dto/BookRequest.java
@@ -2,9 +2,11 @@ package com.testproject.bookservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BookRequest {
     @NotBlank(message = "Isbn is mandatory")
     @Pattern(regexp = "^97[89]\\d{10}$", message = "Incorrect ISBN-13 format")
